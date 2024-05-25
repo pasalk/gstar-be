87d665878c1d4df937dc015c44f3423e3652c1e4
psalkovic <paula.salkovic@fer.hr>
2024-05-25 18:22:20 +0200

    project setup

---
+++ src/main/java/hr/fer/inf/sus/gstarbe/GstarBeApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.inf.sus.gstarbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GstarBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GstarBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/fer/inf/sus/gstarbe/GstarBeApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.inf.sus.gstarbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GstarBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
